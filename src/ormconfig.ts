83d279c516633bd0553ba414b06521c38f31406e
ivan.hryshko <ihryshko@datrics.ai>
2023-09-03 17:52:22 +0300

    refactor: remove useless db keys

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,15 +6,9 @@ export default class TypeOrmConfig {
   static getConfig(configService: ConfigService): PostgresConnectionOptions {
     return {
       type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USER'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_DATABASE'),
-      url: configService.get('DB_URI'),
+      url: configService.get('DATABASE_URL'),
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // This for development
-      // autoLoadEntities: true,
       // ssl: {
       //   rejectUnauthorized: false,
       // },
