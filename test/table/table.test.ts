48a1d26a311b2e53cf28760ad1d6336752c1dad9
ivan.hryshko <ihryshko@datrics.ai>
2023-11-19 13:55:22 +0200

    test: added test

--- test/table/table.test.ts
+++ test/table/table.test.ts
@@ -1,11 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import supertest, * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
 import { TableModule } from '../../src/table/table.module';
 
 describe('TableController (e2e)', () => {
   let app: INestApplication;
+  let authToken: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,6 +20,17 @@ describe('TableController (e2e)', () => {
   it('/api/v1/tables (POST), unauthorized, ', () => {
     return request(app.getHttpServer())
       .post('/api/v1/table')
+      .expect(401)
+  });
+
+  it('/api/v1/tables (POST), unauthorized, ', async () => {
+    const response = await request(app.getHttpServer()).get('/api/v1/users/login');
+    console.log('response :>> ', response.body);
+      authToken = response.body.token;
+      console.log('authToken :>> ', authToken);
+    return request(app.getHttpServer())
+      .post('/api/v1/table')
+      .set('Authorization', `Bearer ${authToken}`)
       .expect(401)
   });
 });
